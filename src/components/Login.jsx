0b2c76ce0c6215c060008fb7d084f7bbf6f0835b
Paulo Vitor Santos Da Silva <131775290+pauloovitorr@users.noreply.github.com>
2024-08-22 22:47:15 -0300

    Primeiro commit

---
+++ src/components/Login.jsx
@@ -0,0 +1,18 @@
+
+export default function Login(){
+    return(
+        <div className="container_login">
+            <div className="pai_img">
+                <div>
+                    <h2>Bem-vindo de Volta!</h2>
+                    <span>Acesse sua conta agora mesmo.</span>
+                </div>
+                <img src="/login.jpg" alt="Imagem de login" />
+            </div>
+
+            <div>
+                
+            </div>
+        </div>
+    )
+}
