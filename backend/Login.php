fb27c5dcd8e70ada4710074d919b081f3b7b5d6d
pauloovitorr <paulov.pv25@gmail.com>
2024-08-24 23:53:13 -0300

    Css para o carregamento e primeira requisição

---
+++ backend/Login.php
@@ -0,0 +1,51 @@
+<?php
+
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS, DELETE, PUT");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit;
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  
+    $json = file_get_contents('php://input');
+    
+
+    $dados = json_decode($json, true);
+
+    $email = $dados['emaill'] ?? '';
+    $senha = $dados['senhaa'] ?? '';
+
+
+    $email_correto = 'paulo@gmail.com';
+    $senha_correta = '123';
+
+    if (!empty($email) && !empty($senha)) {
+       
+        if($email == $email_correto && $senha == $senha_correta){
+            echo json_encode([
+                'status' => 'success',
+                'message' => 'Login efetuado com sucesso',
+                'email' => $email,
+                'senha' => $senha
+            ]);
+        } else{
+
+            echo json_encode([
+                'status' => 'error',
+                'message' => 'Email ou senha inválidos'
+            ]);
+        }
+
+    } else {
+        echo json_encode([
+            'status' => 'error',
+            'message' => 'Email ou senha vazios.'
+        ]);
+    }
+}
+
+?>
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,97 +1,125 @@
 import "../style_componentes/Login.css";
-import { useEffect } from "react";
+import "nprogress/nprogress.css"; 
+import NProgress from "nprogress";
+import React, { useEffect, useState } from "react";
 
 export default function Login() {
 
-  useEffect(() => {
-    const inputs = document.querySelectorAll('.componente_form input');
+  const [Email, setEmail] = useState('')
+  const [Senha, setSenha] = useState('')
 
+
+  useEffect(() => {
     
+    NProgress.configure({ parent: '.container_login' });
+ 
+    const inputs = document.querySelectorAll('.componente_form input');
     inputs.forEach(input => {
-        input.addEventListener('focus', handleFocus);
-        input.addEventListener('blur', handleBlur);
+      input.addEventListener('focus', handleFocus);
+      input.addEventListener('blur', handleBlur);
     });
 
-
+    
     return () => {
-        inputs.forEach(input => {
-            input.removeEventListener('focus', handleFocus);
-            input.removeEventListener('blur', handleBlur);
-        });
+      inputs.forEach(input => {
+        input.removeEventListener('focus', handleFocus);
+        input.removeEventListener('blur', handleBlur);
+      });
     };
-}, []);
 
+  }, []);
 
-
-const handleFocus = (event) => {
+  const handleFocus = (event) => {
     event.target.parentElement.classList.add('focused');
-};
+  };
 
-const handleBlur = (event) => {
+  const handleBlur = (event) => {
     event.target.parentElement.classList.remove('focused');
-};
+  };
 
-    
-    
+
+
+
+
+  const PostLogin = async (e, email, senha) => {
+    e.preventDefault(); 
+  
+    NProgress.start();
+  
+    let dados = {
+      emaill: email,
+      senhaa: senha
+    };
+  
+    try {
+   
+      const resposta = await fetch('http://localhost/pv_academy/backend/Login.php', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(dados) 
+      });
+  
+      
+      if (resposta.ok) {
+        const res = await resposta.json();
+       
+      } else {
+        console.error('Erro na requisição:', resposta.status);
+      }
+    } catch (error) {
+      console.error('Erro na requisição:', error); 
+    } finally {
+      NProgress.done(); 
+    }
+  };
 
 
 
   return (
     <div className="container_login">
       <div className="pai_img">
-
         <div>
           <h2>Bem-vindo de Volta!</h2>
           <span>Acesse sua conta agora mesmo.</span>
         </div>
-
         <img src="/login.jpg" alt="Imagem de login" />
-
       </div>
 
       <div className="form_login">
-
         <div className="texo_login">
-
           <h1>LOGIN</h1>
           <span>Preencha os campos abaixo</span>
-
         </div>
 
         <div className="formulario_login">
 
-          <form action="">
-
+          <form action="" onSubmit={(e)=> { PostLogin(e,Email, Senha) }}>
             <div className="componente_form">
-
               <div>
                 <label htmlFor="email"><i className="fa-solid fa-envelope"></i></label>
-            </div>  
-              <input type="email" id="email" placeholder="E-mail" required autoComplete="off" />
-              
+              </div>
+              <input type="email" id="email" placeholder="E-mail" required autoComplete="off" onChange={(e)=> { setEmail(e.target.value) }} />
             </div>
 
             <div className="componente_form">
-
-            <div>
-               <label htmlFor="senha"> <i className="fa-solid fa-key"></i></label>
-            </div> 
-
-              <input type="password" id="senha" placeholder="Senha" required autoComplete="off" />
-
+              <div>
+                <label htmlFor="senha"> <i className="fa-solid fa-key"></i></label>
+              </div>
+              <input type="password" id="senha" placeholder="Senha" required autoComplete="off" onChange={(e)=> { setSenha(e.target.value) }} />
             </div>
 
             <div className="componente_form">
-                <a href="">Não tem conta? Cadastre-se agora mesmo!</a>
+              <a href="">Não tem conta? Cadastre-se agora mesmo!</a>
             </div>
 
             <div className="componente_form">
-                <button type=""> ENTRAR </button>
+              <button type="submit">ENTRAR</button>
             </div>
-
 
           </form>
-
+          
         </div>
       </div>
     </div>
